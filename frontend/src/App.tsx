a9c09002a13f944a585cf493315a0c68243775d7
ltmartinelli <lucastrindademartinelli@gmail.com>
2022-07-11 18:25:25 -0300

    First Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
-function App() {
- return(
-  <h1>HELLO WORLD</h1>
- )
+import NotificationButton from "./assets/components/NotificationButton"
+
+function App()
+{
+    return (
+        <>
+            <h1>HELLO WORLD</h1>
+            <NotificationButton />
+        </>
+    )
 }
 
 export default App
